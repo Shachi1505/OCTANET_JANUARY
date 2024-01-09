8145c9429b4d71c83adc9ed53c92b0d581559a8b
Shachi Singhal <122254334+Shachi1505@users.noreply.github.com>
2024-01-09 13:35:19 +0530

    Add files via upload
    I have designed a Landing page for a health and wellbeing website called POSHTIK. Here users can get their personalized diet charts. Know the benefits of eating fruits and vegetables.

---
+++ Octa_Task1/slider.js
@@ -0,0 +1,17 @@
+$('.center').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+
+  $('.card1').tilt({
+    scale: 1.2
+});
+$('.card2').tilt({
+    scale: 1.2
+});
